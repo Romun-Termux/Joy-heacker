ee96c7f3464250729595cf75cc5d23dbe58996df
Termax-Romun <124915795+Romun-Termux@users.noreply.github.com>
2023-02-16 22:49:41 +0600

    Create Joy.py

---
+++ Joy.py
@@ -0,0 +1,21 @@
+#Tor real Abbu Joy
+
+#Tor mare chudi DEC kor
+
+#Fuck U 
+
+#এটা মাথাই রাখবি 
+
+#তোর রিয়েল আব্বু জয়
+
+#চোরের বাচ্চা 
+
+#কুততার বাচ্চার শরম নাই
+
+#চোর মাদারচোদ 
+
+import os
+
+import JRY
+
+JRY.login()
